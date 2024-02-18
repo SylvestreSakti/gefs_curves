50d4c58a816faf30d21bec9a86cee571df5c5efe
Sylvestre Prabakaran <38729191+SylvestreSakti@users.noreply.github.com>
2024-02-18 21:15:25 +0100

    Update draw.py to take into account new version of gefs

--- draw.py
+++ draw.py
@@ -11,7 +11,9 @@ class Tableau :
     def __init__(self,filepath,resolution):
 
         with open(filepath, 'rb') as fp:
-            tableau = pickle.load(fp)
+            gefs_data = pickle.load(fp)
+            tableau = gefs_data["Temp 850hPa"]
+            print(tableau)
 
         echeanceRun = tableau[0][0][0:13]
         self.dateRun = datetime.datetime(int(echeanceRun[0:4]),int(echeanceRun[5:7]),int(echeanceRun[8:10]),int(echeanceRun[11:13]),0)
@@ -26,13 +28,15 @@ class Tableau :
 class Grandeur :
 
     def __init__(self, resolution):
-        self.resolution = resolution
-        self.keyT = []
-        self.keyValues = []
+        self.resolution = resolution # Nb points par step
+        self.keyT = [] # Abscisses t pour chaque run
+        self.keyValues = [] #Ordonnées pour chaque run
+        self.keyValuesMax = []
+        self.keyValuesMin = []
 
-        self.t = []
-        self.values = []
-        self.smoothF = 0
+        self.t = [] # Abscisses t (haute résolution)
+        self.values = [] # Valeurs pour chaque abscisses t
+        self.smoothF = 0 # Fonciton d'interpolation
 
 
     def fill(self, tableau):
@@ -44,7 +48,9 @@ class Grandeur :
 
         for i in range(len(tableau)):
             self.keyT += [float(tableau[i][1])]
-            self.keyValues += [sum(tableau[i][2:32]) / 30]
+            self.keyValues += [sum(tableau[i][2:32]) / 30] # Moyenne
+            self.keyValuesMax += [max(tableau[i][2:32])]  # Max
+            self.keyValuesMin += [min(tableau[i][2:32])]  # Min
 
             if i < len(tableau)-1 :
                 self.t += [self.keyT[i]+r/self.resolution for r in range(self.resolution*(int(tableau[i+1][1])-int(tableau[i][1])))]
@@ -54,12 +60,11 @@ class Grandeur :
 
     def montrerGrandeur(self):
         plt.plot(self.t,self.values)
+        plt.fill_between(self.keyT,self.keyValuesMin,self.keyValuesMax)
         plt.plot(self.keyT, self.keyValues, linestyle='None', marker='.')
+        plt.plot(self.keyT, self.keyValuesMax, linestyle='None', marker='.')
+        plt.plot(self.keyT, self.keyValuesMin, linestyle='None', marker='.')
         plt.show()
 
-
-
-
-
-tableau1 = Tableau("data/2020121518.gefs",4)
+tableau1 = Tableau("data/20240217_18.gefs",4)
 tableau1.montrer()
