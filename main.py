7c4b9a226ae1b4137c473f4a186554084127f5d2
Sylvestre Prabakaran <38729191+SylvestreSakti@users.noreply.github.com>
2024-02-18 18:58:07 +0100

    Added multiple parameters scraping (new version of GEFS file)

--- main.py
+++ main.py
@@ -11,35 +11,44 @@ def cleanhtml(raw_html):
   return cleantext
 
 def scrapTableau(heurerun) :
-    page = urllib.request.urlopen('http://www.meteociel.fr/modeles/gefs_table.php?x=0&y=0&lat=48.8621&lon=2.33936&run='+heurerun+'&ext=fr&mode=0&sort=0')
-    page = str(page.read())
+    gefs_data = {"version" : "0.1","lieu" : "Paris"}
+    modes = ["Temp 850hPa","Temp 500hPa","Pression","Precip","Cumuls","Temp. 2m","Z500","CAPE",
+             "Vent 10m","Vent Rafales 10m","Vent 850hPa","Iso 0"]
+    modes_id = [0,1,2,3,23,7,4,6,11,13,9,14]
+    nom_du_run = ""
 
-    table_start = page.find("""<table class""")
-    table_end = page.find("</table>",table_start)
+    for mode in range(len(modes)) :
 
-    html_table = page[table_start:table_end].split("</tr>")
-    tableau = []
-    for i in range(len(html_table)-2) :
-        run = html_table[i+1].split("</td>")
-        run = [cleanhtml(run[0])]+[float(cleanhtml(html)) for html in run[1:-1]]
-        tableau += [run]
-    
-    echeance = tableau[0][0][0:10]
-    jour_date = echeance.replace("-","")
-    nom_du_run = jour_date + heurerun
+            page = urllib.request.urlopen('http://www.meteociel.fr/modeles/gefs_table.php?x=0&y=0&lat=48.8621&lon=2.33936&run='
+                                          +heurerun+'&ext=fr&mode='+str(modes_id[mode])+'&sort=0')
+            print("Scraping page : " + 'http://www.meteociel.fr/modeles/gefs_table.php?x=0&y=0&lat=48.8621&lon=2.33936&run='
+                                          +heurerun+'&ext=fr&mode='+str(modes_id[mode])+'&sort=0')
+            page = str(page.read())
+
+
+            table_start = page.find("""<table class""")
+            table_end = page.find("</table>",table_start)
+
+            html_table = page[table_start:table_end].split("</tr>")
+            tableau = []
+            for i in range(len(html_table)-2) :
+                run = html_table[i+1].split("</td>")
+                run = [cleanhtml(run[0])]+[float(cleanhtml(html)) for html in run[1:-1]]
+                tableau += [run]
+
+            echeance = tableau[0][0][0:10]
+            jour_date = echeance.replace("-","")
+            nom_du_run = jour_date + "_" + heurerun
+            gefs_data["nom_du_run"] = nom_du_run
+            gefs_data[modes[mode]] = tableau.copy()
 
     with open('data/'+nom_du_run+'.gefs', 'wb') as fp:
-        pickle.dump(tableau, fp)
+        pickle.dump(gefs_data, fp)
 
-    print(tableau)
+    print(gefs_data)
 
 
 scrapTableau("00")
 scrapTableau("06")
 scrapTableau("12")
 scrapTableau("18")
-
-"""
-with open ('outfile', 'rb') as fp:
-    itemlist = pickle.load(fp)
-"""
